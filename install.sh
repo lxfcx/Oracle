1bc0caef5cbfe2a842a122039a4de339f8ebb895
lxfcx <86045613+lxfcx@users.noreply.github.com>
2026-05-07 23:55:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -247,7 +247,7 @@ show_menu() {
 main_menu() {
   while true; do
     show_menu
-    read -r -p "请选择操作 [1/2/0/3]：" CHOICE
+    read -r -p "请选择操作 [1/2/3/0]：" CHOICE
 
     case "$CHOICE" in
       1)
@@ -258,16 +258,16 @@ main_menu() {
         edit_config
         pause
         ;;
-      0)
-        uninstall_clean
-        pause
-        ;;
       3)
         echo "已退出"
         exit 0
         ;;
+      0)
+        uninstall_clean
+        pause
+        ;;
       *)
-        echo "❌ 无效选项，请输入 1、2、0 或 3"
+        echo "❌ 无效选项，请输入 1、2、3 或 0"
         sleep 1
         ;;
     esac
